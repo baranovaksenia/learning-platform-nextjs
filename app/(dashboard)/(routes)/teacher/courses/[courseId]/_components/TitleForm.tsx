b34ff68f0f04a0aa70541f60875ff24658aac49d
Ksenia <baranova-ksenia1990@gmail.com>
2024-02-13 09:57:14 +0200

    setup toast, create api route to update the course title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
@@ -1,7 +1,4 @@
 "use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -12,11 +9,16 @@ import {
 	FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
 import axios from "axios"
 import { Pencil } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { useForm } from "react-hook-form"
 import toast from "react-hot-toast"
+import * as z from "zod"
+
+// Interface for props
 interface TitleFormProps {
 	initialData: {
 		title: string
@@ -24,25 +26,34 @@ interface TitleFormProps {
 	courseId: string
 }
 
+// Define form schema using zod
 const formSchema = z.object({
 	title: z.string().min(1, {
 		message: "Title is required",
 	}),
 })
 
+// Define the TitleForm component
 const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+	// State for editing mode
 	const [isEditing, setIsEditing] = useState(false)
+
+	// Function to toggle edit mode
 	const toggleEdit = () => setIsEditing(current => !current)
 
+	// Get the next.js router
 	const router = useRouter()
 
+	// Initialize the form using react-hook-form
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: initialData,
 	})
 
+	// Destructure form state properties
 	const { isSubmitting, isValid } = form.formState
 
+	// Handle form submission
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
 			await axios.patch(`/api/courses/${courseId}`, values)
@@ -54,8 +65,10 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 		}
 	}
 
+	// Render the TitleForm component
 	return (
 		<div className="mt-6 border bg-slate-100 rounded-md p-4">
+			{/* Title and edit button */}
 			<div className="font-medium flex items-center justify-between">
 				Course Title
 				<Button onClick={toggleEdit} variant="ghost">
@@ -64,17 +77,21 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
 					) : (
 						<>
 							<Pencil className="h-4 w-4 mr-2" />
-							Edit title{" "}
+							Edit title
 						</>
 					)}
 				</Button>
 			</div>
-			{isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+			{/* Display the current title when editing */}
+			{isEditing && (
+				<p className="line-clamp-3 text-sm mt-2 ">{initialData.title}</p>
+			)}
+			{/* Display the form when editing */}
 			{isEditing && (
 				<Form {...form}>
 					<form
 						onSubmit={form.handleSubmit(onSubmit)}
-						className="space-y-4 mt-4"
+						className="space-y-4 mt-4 "
 					>
 						<FormField
 							control={form.control}
